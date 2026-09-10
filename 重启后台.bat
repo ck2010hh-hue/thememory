17d50de1e37c03b0d71e1b55a3d3af96614e42e0
ck2010hh-hue <ck2010hh@gmail.com>
2026-09-10 10:14:29 +0800

    add silent-start vbs and use absolute node path in restart bat

--- 重启后台.bat
+++ 重启后台.bat
@@ -4,5 +4,5 @@ for /f "tokens=5" %%a in ('netstat -ano ^| findstr :8787') do taskkill /F /PID %
 cd /d "D:\华为云相册下载\个人照片视频\site-preview"
 echo Starting The Memory admin server...
 echo Open: http://127.0.0.1:8787/admin.html
-node server.js
+"C:\Users\m1333\.workbuddy\binaries\node\versions\22.22.2-2\node.exe" server.js
 pause
